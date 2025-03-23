24f74ba7586d3b8e0b03b13053f5f6d45a118199
strannikhead <144272555+strannikhead@users.noreply.github.com>
2025-03-24 01:42:23 +0500

    add clickable routes list

--- app/Map.tsx
+++ app/Map.tsx
@@ -9,57 +9,82 @@ import {parseGeoJSONRoutes} from "@/app/convertor";
 const ekb = [56.838011, 60.597474] as LatLngExpression;
 
 function getRandomColor() {
-	var letters = '0123456789ABCDEF';
-	var color = '#';
-	for (var i = 0; i < 6; i++) {
-		color += letters[Math.floor(Math.random() * 16)];
-	}
-	return color;
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
 
 async function loadGeoJSON(url: string) {
-	const response = await fetch(url);
-	const geoJsonData = await response.json();
-	return parseGeoJSONRoutes(geoJsonData);
+    const response = await fetch(url);
+    const geoJsonData = await response.json();
+    return parseGeoJSONRoutes(geoJsonData);
 }
 
 const Map = () => {
 
-	const [routes, setRoutes] = useState<LatLngExpression[][]>([])
-	useEffect(() => {
-		const f = async () => {
-			const geoJSON = await loadGeoJSON("/test_routes.geo.json");
-			const arr = [] as LatLngExpression[][]
-			geoJSON.map(t => {
-				arr.push(t.points)
-			})
-			setRoutes(arr)
-		}
-		f()
-	}, []);
+    const [routes, setRoutes] = useState<LatLngExpression[][]>([])
+    const [routesInfo, setRoutesInfo] = useState<{ number: string, points: LatLngExpression[] }[]>([])
 
-	useEffect(() => {
-		console.log(routes);
-	}, [routes]);
+    useEffect(() => {
+        const f = async () => {
+            const geoJSON = await loadGeoJSON("/test_routes.geo.json");
+            const arr = [] as LatLngExpression[][]
+            geoJSON.map(t => {
+                arr.push(t.points)
+            })
+            setRoutes(arr)
+            setRoutesInfo(geoJSON.map(t => {
+                return ({number: t.name, points: t.points});
+            }))
+        }
+        f()
+    }, []);
 
-	return <MapContainer center={ekb} zoom={13} style={{height: "100vh", width: "100%"}}
-											 crs={CRS.EPSG3395}>
-		<TileLayer
-			url='https://core-renderer-tiles.maps.yandex.net/tiles?l=map&x={x}&y={y}&z={z}&scale=1&lang=ru_RU'
-			subdomains={['01', '02', '03', '04']}
-			attribution='&copy; <a http="https://yandex.ru" target="_blank">Yandex</a> contributors'
-			// onAdd={() => handleTileClick_addYandex()}
-			// onRemove={() => handleTileClick_removeYandex()}
-		/>
-		{routes.map((t, i) =>
-			<Polyline key={i} pathOptions={{color: getRandomColor(), weight: 4}} positions={t}/>
-		)}
-		<Marker position={[56.838011, 60.597474]}>
-			<Popup>
-				Это центр ЕКБ
-			</Popup>
-		</Marker>
-	</MapContainer>
+    useEffect(() => {
+        console.log(routes);
+    }, [routes]);
+
+    return (
+        <div style={{position: "relative"}}>
+            <div style={{
+                position: "absolute",
+                top: 10,
+                right: 10,
+                background: "white",
+                padding: "5px",
+                border: "1px solid black",
+                zIndex: 1000
+            }}>
+                <h3>Маршруты</h3>
+                {routesInfo.map((route, index) => (
+                    <button key={index} style={{display: "block", margin: "5px 0"}}
+                            onClick={() => setRoutes([route.points])}>
+                        Маршрут {route.number}
+                    </button>
+                ))}
+            </div>
+            <MapContainer center={ekb} zoom={13} style={{height: "100vh", width: "100%"}}
+                          crs={CRS.EPSG3395}>
+                <TileLayer
+                    url='https://core-renderer-tiles.maps.yandex.net/tiles?l=map&x={x}&y={y}&z={z}&scale=1&lang=ru_RU'
+                    subdomains={['01', '02', '03', '04']}
+                    attribution='&copy; <a http="https://yandex.ru" target="_blank">Yandex</a> contributors'
+                    // onAdd={() => handleTileClick_addYandex()}
+                    // onRemove={() => handleTileClick_removeYandex()}
+                />
+                {routes.map((t, i) =>
+                    <Polyline key={i} pathOptions={{color: getRandomColor(), weight: 4}} positions={t}/>
+                )}
+                <Marker position={[56.838011, 60.597474]}>
+                    <Popup>
+                        Это центр ЕКБ
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </div>);
 };
 
 export default Map;
