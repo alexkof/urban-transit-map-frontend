6b72052775d0494443124514e5d03c2abb6434c1
wswfws <wswfws@ya.ru>
2025-04-18 12:02:37 +0500

    finish create_route_references

--- helpers/create_route_references.py
+++ helpers/create_route_references.py
@@ -1,8 +1,9 @@
 import json
-from collections import defaultdict
 
-def round_coord(coord, decimal_places=9):
+
+def round_coord(coord, decimal_places=12):
     return tuple(round(x, decimal_places) for x in coord)
+
 
 def create_route_references(routes_file, segments_file, output_file):
     """
@@ -22,17 +23,17 @@ def create_route_references(routes_file, segments_file, output_file):
         coords = feature['geometry']['coordinates']
         segment_id = feature['id']
 
-
-
         # Прямое направление
         point1 = tuple(round_coord(coords[0]))
-        point2 = tuple(round_coord(coords[-1]))
-        segment_dict[(point1, point2)]= (segment_id, False, len(coords))
+        point2 = tuple(round_coord(coords[1]))
+        segment_dict[(point1, point2)] = (segment_id, False, len(coords))
 
         # Обратное направление
         point1 = tuple(round_coord(coords[-1]))
         point2 = tuple(round_coord(coords[-2]))
         segment_dict[(point1, point2)] = (segment_id, True, len(coords))
+
+    # print(*segment_dict.items(), sep='\n')
 
     # Обрабатываем маршруты
     for route in routes['features']:
@@ -43,19 +44,20 @@ def create_route_references(routes_file, segments_file, output_file):
 
         while i < n - 1:
             point1 = tuple(round_coord(route_coords[i]))
-            point2 = tuple(round_coord(route_coords[i+1]))
+            point2 = tuple(round_coord(route_coords[i + 1]))
             matches = segment_dict.get((point1, point2), [])
 
             if matches:
-                print(matches)
+                # print(matches)
                 segment_id, is_reversed, length = matches
                 route_segments.append({
                     'segment_id': segment_id,
                     'is_reversed': is_reversed,
                 })
-                i += length
+                i += length - 1
             else:
                 print(f"Предупреждение: не найден сегмент для точки {i} в маршруте {route['properties'].get('Number')}")
+                print(point1, point2)
                 i += 1
 
         route['properties']['segment_refs'] = route_segments
@@ -66,9 +68,10 @@ def create_route_references(routes_file, segments_file, output_file):
 
     print(f"Обработано {len(routes['features'])} маршрутов. Результат в {output_file}")
 
+
 if __name__ == "__main__":
     create_route_references(
         routes_file='public/all_routes_current.geo.json',
         segments_file='public/all_lines_with_ids.geo.json',
-        output_file='public/routes_with_segment_refs.geojson'
+        output_file='public/routes_with_segment_refs.geo.json'
     )
